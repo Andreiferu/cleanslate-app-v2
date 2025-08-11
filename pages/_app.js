2afa4122759b882c2721f0f90be70e38e91a6e13
Andreiferu <98920687+Andreiferu@users.noreply.github.com>
2025-08-11 22:37:21 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,14 +3,14 @@ import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
-    // Register service worker
+    // Register service worker quietly in background
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker.register('/sw.js')
         .then((registration) => {
-          console.log('SW registered: ', registration);
+          console.log('PWA: Service Worker registered');
         })
         .catch((registrationError) => {
-          console.log('SW registration failed: ', registrationError);
+          console.log('PWA: Service Worker registration failed');
         });
     }
   }, []);
@@ -20,19 +20,19 @@ export default function App({ Component, pageProps }) {
       <Head>
         {/* PWA Meta Tags */}
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#667eea" />
+        <meta name="theme-color" content="#3b82f6" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="CleanSlate" />
         
         {/* Apple Touch Icons */}
-        <link rel="apple-touch-icon" href="/icons/icon-152x152.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="/icons/icon-152x152.png" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/icons/icon-192x192.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/icons/icon-192x192.png" />
         
         {/* Favicon */}
-        <link rel="icon" type="image/png" sizes="32x32" href="/icons/icon-72x72.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/icons/icon-72x72.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/icon-192x192.png" />
       </Head>
       <Component {...pageProps} />
     </>
